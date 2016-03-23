99f90221cee2f592c16608f4027ac2a307f1b2e5
Jazmine Zurita <jazzurita1@gmail.com>
2016-03-23 16:57:17 -0400

    File /Users/Jazz/Documents/workspace2/Assignment3/src/BufferPool.java changed at Wed Mar 23 16:57:17 EDT 2016

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -1,3 +1,5 @@
+import java.io.RandomAccessFile;
+
 /**
  * 
  */
@@ -7,5 +9,51 @@
  *
  */
 public class BufferPool {
-
+		//one buffer that manages two files
+		//param in buffer: file, number to read (4), byte_pos (where it is)
+		//byte_pos => x(pos)/1024(block size)
+		//find loc of byte = byte_pos % block_size
+		//byte_pos = blocknum * block_size + posInBlock
+		//client would give an array and you can stuff it in, instead of making a new array
+		//store block num and file it came from
+		//make an array (circular array)
+		//insert on bottom take from top
+		//what if u use something already in buffer pool? have to swap 
+		// each block is 4096 bytes
+	/**
+	 * Constructor for the bufferpool
+	 */
+	public BufferPool()
+	{}
+	
+	//first thing to do is take info and put it into buffers
+	
+	/**
+	 * Inserts into the buffer from the file
+	 */
+	public void insertF(RandomAccessFile file, int numBytesRead, int bytePos) //need a dirty bit
+	{} //might be two inserts 
+	
+	/**
+	 * Inserts into the array
+	 */
+	public void insertA(boolean dbit)
+	{}
+	
+	
+	/**
+	 * 
+	 * @param file the file to read
+	 * @param numBytesRead will always be 4 for this project
+	 * @param bytePos the position of the byte
+	 */
+	public void getBytes(RandomAccessFile file, int numBytesRead, int bytePos)
+	{
+		//figure out if its in the buffer pool or not
+		//if it is don't touch file
+		int loc = bytePos % 1024;
+		//find the block number to determine if it is in buffer pool
+		//if it is, access
+		//if it is not in the buffer pool ???????? (access the file)
+	}
 }
