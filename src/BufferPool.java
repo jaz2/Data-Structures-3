772306f71e9d3d5014a1f2bf1358d72413d2ed5f
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-02 16:05:35 -0400

    File /Users/Jazz/Documents/workspace2/Assignment3/src/BufferPool.java changed at Sat Apr 02 16:05:35 EDT 2016

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -204,7 +204,7 @@ public class BufferPool {
 		{
 			bu.file.seek(bu.block * 4096);
 			bu.file.write(bu.data);
-			System.out.println("Writes: " + bu.file.read(bu.data));
+			System.out.println("Writes: " + bu.data[0]);
 			bu.dbit = false;
 			writes++;
 		}
