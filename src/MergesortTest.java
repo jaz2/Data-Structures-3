80a901caa6ebe44b6ea89ba0062a029beaf47060
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-05 16:15:49 -0400

    File /Users/Jazz/Documents/workspace2/Assignment3/src/MergesortTest.java changed at Tue Apr 05 16:15:49 EDT 2016

--- src/MergesortTest.java
+++ src/MergesortTest.java
@@ -62,35 +62,4 @@ extends TestCase {
 		
 		assertTrue(c.checkFile(args[0]));
 	}
-	
-	/**
-	 * Tests with the b file
-	 * @throws Exception
-	 */
-	@Test
-	public void testSortB() throws Exception
-	{
-		String[] args = new String[3];
-		args[0] = "inputa4.txt";
-		args[1] = "1"; // Buffer pool size
-		args[2] = "statFile.txt";
-		//" a"
-		FileGenerator f = new FileGenerator();
-		CheckFile c = new CheckFile();
-		String[] args2 = new String[3];
-		args2[0] = "-b";
-		args2[1] = args[0]; 
-		args2[2] = args[1];
-		f.generateFile(args2);
-		
-		//(new FileGenerator()).generateFile(args);
-		//RandomAccessFile temp = new RandomAccessFile("test.txt", "rw");
-		//RandomAccessFile file = new RandomAccessFile(args[0], "rw");
-		//call merge sort on file generate and give it a name
-		//Mergesort.sort(file, temp, 0, (int)file.length()/ 4);
-		//check file
-		Mergesort.main(args);
-		
-		assertTrue(c.checkFile(args[0]));
-	}
 }
