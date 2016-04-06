b4a1f56fa2df2d866f70405f45b5ab3092f6a19d
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-06 16:03:11 -0400

    File /Users/Jazz/Documents/workspace2/Assignment3/src/BufferPoolTest.java changed at Wed Apr 06 16:03:11 EDT 2016

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -61,6 +61,7 @@ public class BufferPoolTest extends TestCase {
 	public void testRead() throws IOException
 	{
 		RandomAccessFile f = new RandomAccessFile("new", "rw");
+		f.writeChars("B Y B I");
 		byte bytes[] = new byte[4];
 		byte b[] = {22, 59, 38, 80};
 		byte a[] = {4, 38, 66, 79, 59};
