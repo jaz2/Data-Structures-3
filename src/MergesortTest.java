0ce26f07c6ea8774e5f81f32c561b250af282713
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-06 16:38:29 -0400

    File /Users/Jazz/Documents/workspace2/Assignment3/src/MergesortTest.java changed at Wed Apr 06 16:38:29 EDT 2016

--- src/MergesortTest.java
+++ src/MergesortTest.java
@@ -71,7 +71,7 @@ extends TestCase {
 		RandomAccessFile t = new RandomAccessFile("tfile", "rw");
 		CheckFile c = new CheckFile();
 		BufferPool buf = new BufferPool(3);
-		f.writeChars("GYFHAJCB");
+		f.writeChars("IPFHAJCBUXZY");
 		byte bytes[] = new byte[4];
 		byte b[] = {88, 70, 73, 80};
 		byte a[] = {66, 78, 66, 79};
