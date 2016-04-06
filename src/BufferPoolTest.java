88862cf59beff81a53e5cd5435f9af78111f6bd1
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-06 11:50:15 -0400

    File /Users/Jazz/Documents/workspace2/Assignment3/src/BufferPoolTest.java changed at Wed Apr 06 11:50:15 EDT 2016

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -75,7 +75,7 @@ public class BufferPoolTest extends TestCase {
 		buf = new BufferPool(3);
 		//System.out.println(buf.blox[2].data[1]);
 		//assertFalse(Arrays.equals(b, bytes));
-		assertNull(bytes[1]);
+		assertEquals(f.readLine(), null);
 	}
 	
 	/**
