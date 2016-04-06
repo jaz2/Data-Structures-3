e6c34b2d4671b99ab2122e00ba82e7e544a107ca
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-06 16:16:17 -0400

    File /Users/Jazz/Documents/workspace2/Assignment3/src/MergesortTest.java changed at Wed Apr 06 16:16:17 EDT 2016

--- src/MergesortTest.java
+++ src/MergesortTest.java
@@ -1,5 +1,7 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -61,5 +63,31 @@ extends TestCase {
 		Mergesort.main(args);
 		
 		assertTrue(c.checkFile(args[0]));
+	}
+	
+	public void testSortTo() throws Exception
+	{
+		RandomAccessFile f = new RandomAccessFile("sortfile", "rw");
+		RandomAccessFile t = new RandomAccessFile("tfile", "rw");
+		CheckFile c = new CheckFile();
+		BufferPool buf = new BufferPool(3);
+		f.writeChars("ABCD");
+		byte bytes[] = new byte[4];
+		byte b[] = {88, 59, 38, 80};
+		byte a[] = {66, 38, 66, 79};
+		buf.write(f, 4, 0, b);
+		buf.write(f, 4, 5000, b);
+		buf.write(f, 4, 9000, b);
+		buf.write(f, 4, 0, a);
+		buf.read(f, 4, 0, bytes);
+		assertTrue(Arrays.equals(a, bytes));
+		buf.read(f, 4, 5000, bytes);
+		buf.read(f, 4, 9000, bytes);
+		Mergesort.sort(f, t, 0, (int)f.length()/4);			
+		buf = new BufferPool(3);
+		//System.out.println(buf.blox[2].data[1]);
+		//assertFalse(Arrays.equals(b, bytes));
+		assertTrue(c.checkFile("sortfile"));
+		f.close();
 	}
 }
